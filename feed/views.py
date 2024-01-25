381ae5582374e8a95649d735410c9ba3fd3c6ee3
Mick Miller <mick@cloud-smith.io>
2024-01-24 19:08:57 -0700

    Backend setup

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomePage(TemplateView):
+    template_name = 'home.html'
+
+class Vehicle(TemplateView):
+    template_name = 'vehicle.html'
+
+class Fuel(TemplateView):
+    template_name = 'fuel.html'
+
+class Parts(TemplateView):
+    template_name = 'parts.html'
+
+class Testing(TemplateView):
+    template_name = 'testing.html'
